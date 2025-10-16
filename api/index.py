4ee4ee99527818a0660c9f241440527963973570
Surabhi Mundada <23f2003781@ds.study.iitm.ac.in>
2025-10-17 00:38:44 +0530

    deployment errors fix

--- api/index.py
+++ api/index.py
@@ -1,5 +1,12 @@
-# Vercel serverless function entry point
+from fastapi import FastAPI
+import sys
+import os
+
+# Add the project root to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Import the app
 from core_engine.server import app
 
-# This is required for Vercel
-handler = app
+# Export for Vercel
+app = app
